0d215f18acd1740f3a44f159c442022bd4ffe31d
arjunamg <arjunamg1993@gmail.com>
2018-02-26 23:57:04 +0530

    Router Changes For Login ANd Signup

---
+++ FrountEnd/src/app/app.route.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule,Routes} from '@angular/router';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+
+const routes : Routes=[
+    { path:'', redirectTo:'/login', pathMatch:'full'},
+    { path: 'login', component:LoginComponent },
+    { path : 'signup', component:SignupComponent}  
+    
+]
+
+@NgModule({
+  imports: [
+    
+    RouterModule.forRoot(routes),
+    CommonModule,
+  ],
+  exports:[RouterModule],
+  declarations: []
+})
+export class AppRouteModule { }
